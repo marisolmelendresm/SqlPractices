8dc3f0d8a4d6c181554993cd5692249b650af1d5
Marisol Melendres <73544651+marisolmelendresm@users.noreply.github.com>
2022-04-18 09:17:34 -0500

    Create practice3.sql

---
+++ practice3.sql
@@ -0,0 +1,3 @@
+1. Existen operadores aritméticos en sql para casi cualquier tipo de operación, como raices, factoriales y potencias.
+2. Se pueden crear columnas con valores calculados
+3. AS significa que el nombre de la columna será el que se asigne
